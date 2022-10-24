5e590ee2a98fc4d510ad150589df958c8524ce82
CHRISTOFER NASCIMENTO <11094619922@educacional.up>
2022-10-24 11:51:28 -0300

    Initial

---
+++ app/src/main/java/com/example/correcorre/Models/Corridas.kt
@@ -0,0 +1,5 @@
+package com.example.correcorre.Models
+
+data class Corridas(val nome : String?, val carro : String?, val Categoria : String?, val idade : Int?,
+                    val telefone : String?, val whp : String?)
+
---
+++ app/src/main/java/com/example/correcorre/Models/Usuarios.kt
@@ -0,0 +1,6 @@
+package com.example.correcorre.Models
+
+import com.google.firebase.Timestamp
+
+data class Usuarios(val distancia: String?, val localizacao : String?, val Oponente : String,
+                    val duracao : Int?, val Horario : Timestamp)
